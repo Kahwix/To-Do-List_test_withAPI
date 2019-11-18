8b26d01d49cc8de41d3d8cf4614cc57d35d9bd7e
52999103 <kahwaji.anthony@gmail.com>
2019-11-19 00:05:40 +0100

    get link the controllers todoItem with the routers !

--- routes/index.js
+++ routes/index.js
@@ -15,13 +15,24 @@ router.get("/api", (req, res) =>
 	})
 );
 
-router.post("/api/todos", todosController.create);
 router.get("/api/todos", todosController.list);
+router.post("/api/todos", todosController.create);
 
 router.get("/api/todos/:todoId", todosController.retrieve);
-router.post("/api/todos/:todoId/items", todoItemsController.create);
-
 router.put("/api/todos/:todoId", todosController.update);
 router.delete("/api/todos/:todoId", todosController.destroy);
+
+router.post("/api/todos/:todoId/items", todoItemsController.create);
+router.put("/api/todos/:todoId/items/:todoItemId", todoItemsController.update);
+router.delete(
+	"/api/todos/:todoId/items/:todoItemId",
+	todoItemsController.destroy
+);
+
+router.all("/api/todos/:todoId/items", (req, res) =>
+	res.status(405).send({
+		message: "Method Not Allowed"
+	})
+);
 
 module.exports = router;
