1b06e79176b76cb2860470ba94ff6ef998c6970d
52999103 <kahwaji.anthony@gmail.com>
2019-11-18 16:21:09 +0100

    link todoItem model with Todo ! And link the routers git add --all!

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 import express from "express";
 const router = express.Router();
+const todosController = require("../server/controllers").todos;
+const todoItemsController = require("../server/controllers").todoItems;
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
 	res.render("index", { title: "Express" });
 });
+
+/* 2nd Page */
+router.get("/api", (req, res) =>
+	res.status(200).send({
+		message: "Welcome to the Todos API!"
+	})
+);
+
+router.post("/api/todos", todosController.create);
+router.get("/api/todos", todosController.list);
+
+router.post("/api/todos/:todoId/items", todoItemsController.create);
 
 module.exports = router;
