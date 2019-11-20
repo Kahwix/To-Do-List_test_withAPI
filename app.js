5c133ff7ad05c2895c5d4cf3b26966cdc9c72b92
52999103 <kahwaji.anthony@gmail.com>
2019-11-20 09:09:55 +0100

    update backend, for adding the middlerware : cors in app.js & use it for link with vue-todo

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import createError from "http-errors";
 import express from "express";
+import cors from "cors";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
